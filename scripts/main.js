db944f3c7c85be4117cb42b1871afda40eb13f8a
Ashley M Furin <63980287+amfurin@users.noreply.github.com>
2020-12-05 19:33:07 -0500

    Add existing website folders and files via upload

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome to our farm, ' + myName + '!';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to our farm, ' + storedName + '!';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
